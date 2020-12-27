949b9a2db3a3b9550145cee8aa4d1af1cb71d6fb
roman <romangaral@gmail.com>
2020-12-27 18:06:15 +0100

    creando login

--- src/main/java/com/curso/web/app/controller/LoginController.java
+++ src/main/java/com/curso/web/app/controller/LoginController.java
@@ -5,14 +5,16 @@ import java.security.Principal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
 
 	@GetMapping("/login")
-	public String login(Model model, Principal principal) {
+	public String login(Model model, Principal principal, RedirectAttributes flash) {
 		
 		if(principal != null) {
+			flash.addFlashAttribute("info", "Ya ha iniciado sesion");
 			return "redirect:/";
 		}
 		
